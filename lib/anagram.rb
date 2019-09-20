312a434d3b93f7439fe2d5bbc4b3008323319209
Matthew C <m23.cullen@gmail.com>
2019-09-20 18:08:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,16 @@
 class Anagram
-
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(array)
+    array.each do |word|
+      word_array = word.split("").sort 
+      if word_array == @word.split 
+        word 
+      end 
+    end 
+  end 
 end 
