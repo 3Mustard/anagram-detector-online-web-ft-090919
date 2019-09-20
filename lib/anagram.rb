9a4a3ff488984deec255fc84a345017a31af04d0
Matthew C <m23.cullen@gmail.com>
2019-09-20 18:11:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,9 +6,10 @@ class Anagram
   end 
   
   def match(array)
+    word = @word.split("")
     array.collect do |word|
       split_array = word.split("")
-      if split_array.sort == @word.sort 
+      if split_array.sort == word.sort
         word
       end 
     end 
